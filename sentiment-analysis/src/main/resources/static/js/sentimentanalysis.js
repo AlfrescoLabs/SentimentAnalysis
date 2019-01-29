5834b3f7634ac54b362eb342147097a50947cdbc
Charles Le Seach <cleseach@amexio.fr>
2019-01-29 20:24:14 +0100

    Bees are coloured following the sentiment Choose of the algorithm

--- sentiment-analysis/src/main/resources/static/js/sentimentanalysis.js
+++ sentiment-analysis/src/main/resources/static/js/sentimentanalysis.js
@@ -1,30 +1,48 @@
- window.addEventListener("load", function () {
-      function callAnalyze() {
-        var XHR = new XMLHttpRequest();
-        var FD = new FormData(form);
-        XHR.addEventListener("load", function(event) {
-          var responseJson = JSON.parse(event.target.response);
-          positiverank.innerHTML = responseJson.ranking.positive;
-          negativerank.innerHTML = responseJson.ranking.negative;
-          neutralrank.innerHTML = responseJson.ranking.neutral;
-          results.style.display='block';
-        });
-        XHR.addEventListener("error", function(event) {
-          alert('Oups! Quelque chose s\'est mal passé.');
-        });
-        XHR.open("POST", "/analyze/text");
-        XHR.send(text.value);
-        results.style.display='none';
-      }
+function displayRanking(object, ranking) {
+    var svgDoc = object.contentDocument;
+    var element = svgDoc.getElementById('ranking');
+    element.textContent =  ranking;
+}
+
+window.addEventListener("load", function () {
       var form = document.getElementById("analyze");
       var text = document.getElementById('text');
       var results = document.getElementById('results');
       var library = document.getElementById('library');
-      var positiverank = document.getElementById('positiverank');
-      var negativerank = document.getElementById('negativerank');
-      var neutralrank = document.getElementById('neutralrank');
+      var positivebee = document.getElementById('positivebee');
+      var negativebee = document.getElementById('negativebee');
+      var neutralbee = document.getElementById('neutralbee');
       form.addEventListener("submit", function (event) {
         event.preventDefault();
         callAnalyze();
       });
+
+    function updateBeeRanking(bee, ranking) {
+        if (bee.contentDocument) {
+            displayRanking(bee, ranking);
+        } else {
+            bee.addEventListener("load",function() {
+                displayRanking(this, ranking);
+            }, false);
+        }
+    }
+
+    function callAnalyze() {
+        var XHR = new XMLHttpRequest();
+        var FD = new FormData(form);
+        XHR.addEventListener("load", function(event) {
+            var responseJson = JSON.parse(event.target.response);
+            updateBeeRanking(positivebee, responseJson.ranking.positive);
+            updateBeeRanking(neutralbee, responseJson.ranking.neutral);
+            updateBeeRanking(negativebee, responseJson.ranking.negative);
+            results.style.visibility='visible';
+        });
+        XHR.addEventListener("error", function(event) {
+          alert('Oups! Somehting went wrong.');
+        });
+        XHR.open("POST", "/analyze/" + library.value + "/text");
+        XHR.send(text.value);
+        results.style.visibility='hidden';
+    }
+
 });

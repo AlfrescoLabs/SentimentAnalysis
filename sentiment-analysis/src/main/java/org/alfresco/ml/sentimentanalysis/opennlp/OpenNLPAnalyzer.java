454c75d7b17934a083df902fb553f38452afe575
Nick Burch <nick@gagravarr.org>
2019-01-29 16:38:59 +0000

    Load the OpenNLP model

--- sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
+++ sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
@@ -1,5 +1,9 @@
 package org.alfresco.ml.sentimentanalysis.opennlp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.alfresco.ml.sentimentanalysis.SentimentAnalyzer;
 import org.alfresco.ml.sentimentanalysis.model.Ranking.ANALYSIS_OUTCOME;
 
@@ -11,12 +15,20 @@ public class OpenNLPAnalyzer implements SentimentAnalyzer
     private DocumentCategorizerME categorizer;
 
     // TODO How to configure our model path?
-    // TODO When to load the model?
+    public static final String modelPath = 
+               "src/main/resources/models/opennlp-sentiments.model";
+    public OpenNLPAnalyzer() throws IOException 
+    {
+        FileInputStream input = new FileInputStream(new File(modelPath));
+        DoccatModel model = new DoccatModel(input);
+        categorizer = new DocumentCategorizerME(model);
+    }
 
     @Override
     public ANALYSIS_OUTCOME analyzeLine(String line)
     {
         // Split on whitespace or some punctuation
+        // TODO Use an OpenNLP tokenizer to do this for us
         String[] tokens = line.split("[\\w\\.,]");
 
         // Categorise

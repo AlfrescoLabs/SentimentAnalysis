a8d2acbfb1bb3b8f410a79bb9478bdcfeae54753
Nick Burch <nick@gagravarr.org>
2019-01-29 14:10:57 +0000

    Track sentiment

--- sentiment-analysis/src/main/groovy/JHUSentimentReader.groovy
+++ sentiment-analysis/src/main/groovy/JHUSentimentReader.groovy
@@ -4,6 +4,9 @@ import opennlp.tools.doccat.DocumentSample
 import opennlp.tools.util.ObjectStream
 
 public class JHUSentimentReader implements ObjectStream<DocumentSample> {
+   public static final String POSITIVE = "Positive"
+   public static final String NEGATIVE = "Negative"
+
    private File folder
    private List<File> reviews
    private List<DocumentSample> samples
@@ -28,10 +31,18 @@ public class JHUSentimentReader implements ObjectStream<DocumentSample> {
       if (samples.size() == 0) {
          if (reviews.size() == 0) return null
 
-         File file = reviews.removeLast()
-         println file
+         File file = reviews.remove(reviews.size()-1)
+         String sentiment = file.getName().startsWith("pos") ? 
+                            POSITIVE : NEGATIVE
+println "${sentiment} ${file}"
+         file.eachLine { line ->
+         }
+      }
+
+      if (samples.size() > 0) {
+         return samples.remove(samples.size()-1)
+      } else {
          return null
       }
-      return samples.removeLast()
    }
 }

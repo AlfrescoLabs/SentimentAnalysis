6d14a0645f636430a61d4359506994f467bcd1de
Nick Burch <nick@gagravarr.org>
2019-01-29 11:51:29 +0000

    Specify where to get training data from

--- sentiment-analysis/src/main/groovy/TrainOpenNLP.groovy
+++ sentiment-analysis/src/main/groovy/TrainOpenNLP.groovy
@@ -1,6 +1,20 @@
 #!/usr/bin/env groovy
 
 @Grab('org.apache.opennlp:opennlp-tools:1.9.1')
+import opennlp.tools.doccat.DoccatModel;
 import opennlp.tools.doccat.DocumentCategorizerME;
+import opennlp.tools.doccat.DocumentSampleStream;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
 
-println "Hello, world" 
+// Which folder to process?
+File folder = new File("processed_acl")
+if (args.length > 0) folder = new File(args[0])
+if (! folder.exists()) {
+   println "Error - training data not found in ${folder}"
+   println "Please download and unpack"
+   println "  http://www.cs.jhu.edu/~mdredze/datasets/sentiment/processed_acl.tar.gz"
+}
+println "Building training data from ${folder}"
+
+// TODO

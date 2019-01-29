f0be40cedb8e47714163594fbd9fae62abb199d6
Nick Burch <nick@gagravarr.org>
2019-01-29 16:20:20 +0000

    Add OpenNLP dependency

--- sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
+++ sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
@@ -3,6 +3,9 @@ package org.alfresco.ml.sentimentanalysis.opennlp;
 import org.alfresco.ml.sentimentanalysis.SentimentAnalyzer;
 import org.alfresco.ml.sentimentanalysis.model.Ranking.ANALYSIS_OUTCOME;
 
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
+
 public class OpenNLPAnalyzer implements SentimentAnalyzer
 {
 

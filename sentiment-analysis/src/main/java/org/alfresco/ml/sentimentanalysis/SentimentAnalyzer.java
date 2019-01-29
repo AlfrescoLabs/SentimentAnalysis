6afd80de5bf52cacea78433e3de7b3ffe745b8ca
Matteo Mazzola <mmazzola@mintel.com>
2019-01-29 11:20:00 +0000

    Interface Analyzer

--- sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/SentimentAnalyzer.java
+++ sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/SentimentAnalyzer.java
@@ -1,44 +1,7 @@
 package org.alfresco.ml.sentimentanalysis;
 
-import java.util.Properties;
+public interface SentimentAnalyzer {
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.util.CoreMap;
+	public String analyze(String line);
 
-public class SentimentAnalyzer {
-
-
-    StanfordCoreNLP tokenizer;
-    StanfordCoreNLP pipeline;
- 
-    public SentimentAnalyzer() {
-        Properties pipelineProps = new Properties();
-        Properties tokenizerProps = new Properties();
-        pipelineProps.setProperty("annotators", "parse, sentiment");
-        pipelineProps.setProperty("parse.binaryTrees", "true");
-        pipelineProps.setProperty("enforceRequirements", "false");
-        tokenizerProps.setProperty("annotators", "tokenize ssplit");
-        tokenizer = new StanfordCoreNLP(tokenizerProps);
-        pipeline = new StanfordCoreNLP(pipelineProps);
-    }
- 
-    public String analyze(String line) {
-        Annotation annotation = tokenizer.process(line);
-        pipeline.annotate(annotation);
-        String output = "";
-        for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
-            output += sentence.get(SentimentCoreAnnotations.SentimentClass.class);
-            output += "\n";
-        }
-        return output;
-    }
-    
-    public static void main(String... args) throws Exception {
-    	    SentimentAnalyzer sa = new SentimentAnalyzer();
-    	    System.out.println(sa.analyze("You have no idea about nothing, you're stupid!"));
-    }
-    
 }

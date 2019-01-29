ce907cb800e730446443e97692bffc240fb836b9
Nick Burch <nick@gagravarr.org>
2019-01-29 16:28:19 +0000

    Implement OpenNLP analysis, but not yet model loading

--- sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
+++ sentiment-analysis/src/main/java/org/alfresco/ml/sentimentanalysis/opennlp/OpenNLPAnalyzer.java
@@ -8,11 +8,29 @@ import opennlp.tools.doccat.DocumentCategorizerME;
 
 public class OpenNLPAnalyzer implements SentimentAnalyzer
 {
+    private DocumentCategorizerME categorizer;
+
+    // TODO How to configure our model path?
+    // TODO When to load the model?
 
     @Override
     public ANALYSIS_OUTCOME analyzeLine(String line)
     {
-        return null;
+        // Split on whitespace or some punctuation
+        String[] tokens = line.split("[\\w\\.,]");
+
+        // Categorise
+        double[] outcomes = categorizer.categorize(tokens);
+
+        // What is that?
+        String category = categorizer.getBestCategory(outcomes);
+
+        // Map to our results
+        if (category.equals("positive"))
+           return ANALYSIS_OUTCOME.POSITIVE;
+        if (category.equals("negative"))
+           return ANALYSIS_OUTCOME.NEGATIVE;
+        return ANALYSIS_OUTCOME.NEUTRAL;
     }
 
 }
